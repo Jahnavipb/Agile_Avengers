7584ae104666f9a792158381b3ed805e3df79dfc
xiong.chen.ur <xiong.chen@ur.com.cn>
2023-03-02 14:50:16 -0500

    Setup

---
+++ src/main/java/cs555/sun_energy/SunEnergyApplication.java
@@ -0,0 +1,13 @@
+package cs555.sun_energy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunEnergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunEnergyApplication.class, args);
+	}
+
+}
