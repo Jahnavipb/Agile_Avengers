13d24ff1da91f92c7a6235be6d26e29e6d2558b3
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-03-28 23:30:53 -0400

    Test case for sign up
    Test case for sign up

---
+++ __tests__/signup.test.js
@@ -0,0 +1,27 @@
+import { getByLabelText, getByText, fireEvent } from '@testing-library/dom';
+import '@testing-library/jest-dom/extend-expect';
+import fs from 'fs';
+import path from 'path';
+
+const html = fs.readFileSync(path.resolve(__dirname, '../signup.html'), 'utf8');
+
+beforeEach(() => {
+  document.documentElement.innerHTML = html.toString();
+});
+
+test('check if the form elements are present', () => {
+  const form = document.querySelector('#signup-form');
+  const emailInput = getByLabelText(form, 'Email');
+  const passwordInput = getByLabelText(form, 'Password');
+  const submitButton = getByText(form, 'Submit');
+
+  expect(emailInput).toBeInTheDocument();
+  expect(passwordInput).toBeInTheDocument();
+  expect(submitButton).toBeInTheDocument();
+});
+
+test('form submit event', () => {
+  const form = document.querySelector('#signup-form');
+  fireEvent.submit(form);
+  // Add your expectations related to form submission here
+});
