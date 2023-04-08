1e799377e8e6ac9e49471cb6c0019e913d4545b8
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-04-08 17:05:19 -0400

    Sign Up page redesigned
    Sign up page redesigned!

--- static/signup.js
+++ static/signup.js
@@ -1,135 +1,93 @@
-import sgMail from '@sendgrid/mail';
-let YOUR_SENDGRID_API_KEY = "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";
-sgMail.setApiKey(YOUR_SENDGRID_API_KEY);
+const form = document.getElementById("signup-form");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const password = document.getElementById("password");
+const retypePassword = document.getElementById("retype-password");
 
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('form');
-  const inputs = document.querySelectorAll('input');
-  const submit = document.querySelector('button[type="submit"]');
-  submit.disabled = true; // Disable the submit button initially
+function validateEmail() {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const valid = emailRegex.test(email.value);
+  email.classList.toggle("is-invalid", !valid);
+  return valid;
+}
 
-  const inputInteracted = {};
+function validatePhone() {
+  const phoneRegex = /^\d{10}$/;
+  const valid = phoneRegex.test(phone.value);
+  phone.classList.toggle("is-invalid", !valid);
+  return valid;
+}
 
-  inputs.forEach(input => {
-    input.addEventListener('input', (e) => {
-      inputInteracted[e.target.name] = true;
-      validateInput(input);
-      checkFormValidity();
-    });
+function validatePasswordsMatch() {
+  const valid = password.value === retypePassword.value;
+  retypePassword.classList.toggle("is-invalid", !valid);
+  return valid;
+}
 
-    input.addEventListener('blur', (e) => {
-      if (inputInteracted[e.target.name]) {
-        validateInput(input);
-      }
-    });
-  });
+email.addEventListener("input", validateEmail);
+phone.addEventListener("input", validatePhone);
+password.addEventListener("input", validatePasswordsMatch);
+retypePassword.addEventListener("input", validatePasswordsMatch);
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const emailIsValid = validateEmail();
+  const phoneIsValid = validatePhone();
+  const passwordsMatch = validatePasswordsMatch();
 
-    let fullName, email, phoneNumber, address, password, retypePassword;
-
-    inputs.forEach(input => {
-      if (input.name === 'full_name') fullName = input.value.trim();
-      if (input.name === 'email') email = input.value.trim();
-      if (input.name === 'phone_number') phoneNumber = input.value.trim();
-      if (input.name === 'address') address = input.value.trim();
-      if (input.name === 'password') password = input.value.trim();
-      if (input.name === 'retype_password') retypePassword = input.value.trim();
-    });
-
-    console.log(fullName, email, phoneNumber, address, password, retypePassword);
-
-    const otp = generateOTP();
-    sendVerificationEmail(email, otp)
-      .then(() => {
-        sessionStorage.setItem('sentOtp', otp.toString()); // Save the OTP to sessionStorage
-        window.location.href = 'verify.html';
-      })
-      .catch((error) => {
-        console.error('Error sending email:', error);
-      })
-      .finally(() => {
-        checkFormValidity();
-      });
-  });
-
-  function validateInput(input) {
-    const inputType = input.getAttribute('type');
-    const value = input.value.trim();
-
-    let isValid = false;
-    let errorMessage = '';
-
-    if (inputType === 'email') {
-      isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
-      errorMessage = 'Please enter a valid email address';
-    } else if (inputType === 'tel') {
-      isValid = /^\d{10}$/.test(value);
-      errorMessage = 'Please enter a valid phone number';
-    } else if (inputType === 'password') {
-      isValid = value !== '';
-      errorMessage = 'Please enter a password';
-    } else {
-      isValid = value !== '';
-      errorMessage = `Please enter ${input.name === 'full_name' ? 'your full name' : 'your address'}`;
-    }
-
-    if (isValid) {
-      input.classList.remove('invalid');
-      input.nextElementSibling.style.display = 'none';
-    } else if (inputInteracted[input.name]) {
-      input.classList.add('invalid');
-      input.nextElementSibling.style.display = 'inline';
-      input.nextElementSibling.textContent = errorMessage;
-    }
-
-    return isValid;
+  if (emailIsValid && phoneIsValid && passwordsMatch) {
+    console.log("Form submitted");
   }
+});
 
-  function checkFormValidity() {
-    let isFormValid = true;
-    let passwordsMatch = true;
+const submitButton = document.getElementById("submit-button");
 
-    inputs.forEach(input => {
-      if (!validateInput(input)) {
-        isFormValid = false;
-      }
+function updateSubmitButtonState() {
+  const emailIsValid = !email.classList.contains("is-invalid");
+  const phoneIsValid = !phone.classList.contains("is-invalid");
+  const passwordsMatch = !retypePassword.classList.contains("is-invalid");
 
-      if (input.name === 'password' || input.name === 'retype_password') {
-        const passwordInput = document.querySelector('input[name="password"]');
-        const retypePasswordInput = document.querySelector('input[name="retype_password"]');
+  submitButton.disabled = !(emailIsValid && phoneIsValid && passwordsMatch);
+}
 
-        if (passwordInput.value.trim() !== retypePasswordInput.value.trim()) {
-          passwordsMatch = false;
-          retypePasswordInput.classList.add('invalid');
-          retypePasswordInput.nextElementSibling.style.display = 'inline';
-          retypePasswordInput.nextElementSibling.textContent = "Passwords don't match";
-        } else {
-          retypePasswordInput.classList.remove('invalid');
-          retypePasswordInput.nextElementSibling.style.display = 'none';
-        }
-      }
-    });
+email.addEventListener("input", () => {
+  validateEmail();
+  updateSubmitButtonState();
+});
 
-    submit.disabled = !(isFormValid && passwordsMatch);
-  }
+phone.addEventListener("input", () => {
+  validatePhone();
+  updateSubmitButtonState();
+});
 
-  async function sendVerificationEmail(email, otp) {
-    const msg = {
-      to: email,
-      from: 'sbhanda7@stevens.edu', // Replace with your verified sender email
-      subject: 'Email Verification',
-      text: `Your verification code is: ${otp}`,
-      html: `<strong>Your verification code is: ${otp}</strong>`,
-    };
+password.addEventListener("input", () => {
+  validatePasswordsMatch();
+  updateSubmitButtonState();
+});
 
-    return sgMail.send(msg);
-  }
+retypePassword.addEventListener("input", () => {
+  validatePasswordsMatch();
+  updateSubmitButtonState();
+});
 
-  function generateOTP() {
-    return Math.floor(100000 + Math.random() * 900000);
-  }
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("Form submitted");
+});
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Collect form field values
+  const formData = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    phone: document.getElementById("phone").value,
+    address: document.getElementById("address").value,
+    password: document.getElementById("password").value,
+  };
+
+  // Log the form data
+  console.log("Form submitted with the following data:", formData);
 });
 
