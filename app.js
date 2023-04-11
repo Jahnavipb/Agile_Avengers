efd7c8faf06d69b7bf8c595853126b3e0b499126
Jahnavi <jahnavipb@gmail.com>
2023-04-11 02:18:40 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // Setting up server
 
+var startTime = performance.now();
+
 const express = require('express');
 const app = express();
 const configRoutes = require('./routes');
+const bookdemo = require('./data/bookdemo');
+const connection = require('./config/mongoConnection');
 
+app.use(express.json());
 configRoutes(app);
 
 
@@ -11,3 +16,23 @@ app.listen(3000, () => {
   console.log("We've now got a server!");
   console.log('Your routes will be running on http://localhost:3000');
 });
+
+const main = async () => {
+  const db = await connection.dbConnection();
+  await db.dropDatabase();
+
+  try{
+    let demo1 = await bookdemo.createDemo("Jahnavi", "jahnavi@gmail.com", "6462886335", "186 Kensington Avenue", "4/12/2023", "14:30");
+    console.log('Your reference ID is : ' + demo1);
+  } catch(e) {
+  console.log(e);
+  }
+
+  await connection.closeConnection();
+  console.log('Done!'); 
+};
+
+ain();
+
+var endTime = performance.now();
+console.log(`Call to do Something took ${endTime - startTime} milliseconds`);
