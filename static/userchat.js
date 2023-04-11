7663f6c80eedec141c20c0289ec585c465be19bb
Venkata Sumanth Nagabhairu <46236771+sumanth2727@users.noreply.github.com>
2023-04-10 20:57:07 -0400

    Add files via upload

---
+++ static/userchat.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    // Customer chat functionality
+    $("#customer-send-button").click(function() {
+      var message = $("#customer-sage-input").val();
+      if (message != "") {
+        $("#customer-message-list").append("<li class='customer-message'>" + message + "</li>");
+        $("#customer-message-input").val("");
+      }
+    });
+  
+    // Sales chat functionality
+    $("#sales-send-button").click(function() {
+      var message = $("#sales-message-input").val();
+      if (message != "") {
+        $("#sales-message-list").append("<li class='sales-message'>" + message + "</li>");
+        $("#sales-message-input").val("");
+      }
+    });
+  
+  });
+  
