cdfe7b6114d7a9c10d3cd86dad67666c7b0cb9f3
Venkata Sumanth Nagabhairu <46236771+sumanth2727@users.noreply.github.com>
2023-04-25 12:56:43 -0400

    Update userchat.js

--- static/userchat.js
+++ static/userchat.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     $("#customer-send-button").click(function() {
       var message = $("#customer-sage-input").val();
       if (message != "") {
+      // check message is empty or not
         $("#customer-message-list").append("<li class='customer-message'>" + message + "</li>");
         $("#customer-message-input").val("");
       }
